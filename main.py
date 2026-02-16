ad504354c269f044e91975f8eaa205f99ab9cbe5
Larnor96 <larshovde.nordaas@gmail.com>
2026-02-16 18:43:33 +0100

    lol smash it go api
    fast api rest api endpoint for the program or life¨more to come mby

--- main.py
+++ main.py
@@ -1,53 +1,23 @@
-tasks = []
+#RESTAPI endpoints
 
-def addTask():
-    task = input("Please enter a task: ")
-    tasks.append(task)
-    print(f"Tasks '{task}' added to the list.")
+from fastapi import FastAPI
 
+app = FastAPI()
+
+taskList = []
+
+@app.get('/')
+def root():
+    return {"hello": "World"}
+
+@app.post('/addTask')
+def addTask(task: str):
+    taskList.append(task)
+    return {"message": f"Task '{task}' added to the list."}
+
+@app.get('/listTasks')
 def listTasks():
-    if not tasks:
-        print("there are no tasks currently.")
+    if not taskList:
+        return {"message": "There are no tasks currently."}
     else:
-        print("Current Tasks:")
-        for index, task in enumerate(tasks):
-            print(f"Task #{index}. {task}")
-
-def deleteTask():
-    listTasks()
-    try:
-        taskToDelete = int(input("Enter the # to delete: "))
-        if taskToDelete >=0 and taskToDelete < len(tasks):
-            tasks.pop(taskToDelete)
-            print(f"Task {taskToDelete} has been removed.")
-        else:
-            print (f"Task #{taskToDelete} was not found .")   
-    except:
-        print("invalid input.")
-
-if __name__ == "__main__":
-    #loop for å kjøre appen
-    print("welcome to the to do list app :)")
-    while True:
-        print("\n")
-        print("Please select one of the following options")
-        print("------------------------------------------")
-        print("1. Add a new task")
-        print("2. Delete a task")
-        print("3. List tasks")
-        print("4. Quit")
-
-        choice = input("Enter your choice: ")
-
-        if(choice == "1"):
-            addTask()
-        elif(choice == "2"):
-            deleteTask()
-        elif(choice == "3"):
-            listTasks()
-        elif(choice == "4"):
-            break
-        else:
-            print("Invalid input. Please try again.")
-
-    Print("Goodbye")
+        return {"tasks": taskList}
