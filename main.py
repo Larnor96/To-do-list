7afa2b713b1489572e03220abcd795a652ae3c1b
Larnor96 <larshovde.nordaas@gmail.com>
2026-02-17 10:58:25 +0100

    apipapi

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #RESTAPI endpoints
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+
+class Task(BaseModel):
+    text: str
+    is_done: bool = False
 
 app = FastAPI()
 
@@ -15,9 +20,14 @@ def addTask(task: str):
     taskList.append(task)
     return {"message": f"Task '{task}' added to the list."}
 
-@app.get('/listTasks')
+@app.get('/listTasks', response_model=list[Task])
 def listTasks():
-    if not taskList:
-        return {"message": "There are no tasks currently."}
+    return taskList
+    
+@app.delete('/deleteTask/{taskIndex}')
+def deleteTask(taskIndex: int):
+    if 0 <= taskIndex < len(taskList):
+        deleted_task = taskList.pop(taskIndex)
+        return {"message": f"Task '{deleted_task}' deleted from the list."}
     else:
-        return {"tasks": taskList}
+        raise HTTPException(status_code=404, detail="Task index is out of bounds.")
